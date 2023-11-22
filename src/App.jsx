9745eb6968b2265307902ef20e8cdace5d0e9fbd
muhiqbalwibowo <muhiqbalwibowo@gmail.com>
2023-11-22 13:55:08 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Final project BM 7</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
